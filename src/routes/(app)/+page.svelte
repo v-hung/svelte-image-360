deeada670f6de8bb6b7560b3ac1fe7903f617212
Viet Hung <viet.hung.2898@gmail.com>
2023-08-18 17:33:11 +0700

    add

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,1 @@
-<!-- xoa swiper thay the bang slide nhe hon
-doi thu muc storage sang storage link
-layzo load components neu can thiet
-kiem tra ham checkIOS tren ios khi chay qua nigix thuc te -->
+<!-- page -->
